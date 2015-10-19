bc4cc37fdc30544a719d5a77ad720e6cfcf3058f
vovans87 <excellentwebmaster@rambler.ru>
2015-10-19 09:22:56 -0700

    commit

---
+++ iproby/iproby/Controllers/CatalogController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace iproby.Controllers
+{
+    public class CatalogController : Controller
+    {
+        //
+        // GET: /Catalog/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
